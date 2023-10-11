4c5210647f2e4793ba666e0ecab26e1c5ff2ae16
ABDULHAM1D0 <133525851+ABDULHAM1D0@users.noreply.github.com>
2023-10-11 09:42:47 +0300

    Update main.py
    active smtlp

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-# Optional: add contact me email functionality (Day 60)
-# import smtplib
+import smtplib
 
 
 
